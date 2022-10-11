502e32b7a50c1a3a44259b8d71bdbafe7aa61c48
Lucas Mancini <lucas@boozebud.com>
2022-10-11 11:17:38 +1100

    Add Nav and basic form to submit a product.

---
+++ sick-fits/frontend/lib/useForm.js
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+
+export default function useForm(initial = {}) {
+  // create in a state object for our inputs
+  const [inputs, setInputs] = useState(initial);
+
+  function handleChange(e) {
+    let { value, name, type } = e.target;
+
+    if (type === 'number') {
+      value = parseInt(value);
+    }
+    if (type === 'file') {
+      [value] = e.target.files;
+    }
+
+    setInputs({
+      ...inputs, // copy the existing state
+      [name]: value,
+    });
+  }
+
+  function resetForm() {
+    setInputs(initial);
+  }
+
+  function clearForm() {
+    const blankState = Object.fromEntries(
+      Object.entries(inputs).map(([key]) => [key, ''])
+    );
+    setInputs(blankState);
+  }
+
+  return { inputs, handleChange, resetForm, clearForm };
+}
