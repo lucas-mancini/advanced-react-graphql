a115d9f2a7fd8f9c22ca4fd6395ef2b941bf5be0
Lucas Mancini <lucas@boozebud.com>
2022-10-12 11:00:33 +1100

    Logic to create new product.

--- sick-fits/frontend/components/CreateProduct.js
+++ sick-fits/frontend/components/CreateProduct.js
@@ -1,22 +1,62 @@
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/client';
+import Router from 'next/router';
+
 import useForm from '../lib/useForm';
 import Form from './styles/Form';
+import DisplayError from './ErrorMessage';
+import { ALL_PRODUCTS_QUERY } from './Products';
+
+const CREATE_PRODUCT_MUTATION = gql`
+  mutation CREATE_PRODUCT_MUTATION(
+    # which variables are getting passed in?
+    $name: String!
+    $description: String!
+    $price: Int!
+    $image: Upload
+  ) {
+    createProduct(
+      data: {
+        name: $name
+        description: $description
+        price: $price
+        status: "AVAILABLE"
+        photo: { create: { image: $image, altText: $name } }
+      }
+    ) {
+      id
+      price
+      description
+      name
+    }
+  }
+`;
 
 export default function CreateProduct() {
-  const { inputs, handleChange } = useForm({
+  const { inputs, handleChange, clearForm } = useForm({
     image: '',
     name: 'Nice shoes',
     price: 342412,
     description: 'These are the best shoes',
   });
 
+  const payload = useMutation(CREATE_PRODUCT_MUTATION, {
+    variables: inputs,
+    refetchQueries: [{ query: ALL_PRODUCTS_QUERY }],
+  });
+  const [createProduct, { error, loading }] = payload;
+
   return (
     <Form
-      onSubmit={(event) => {
+      onSubmit={async (event) => {
         event.preventDefault();
-        console.log(inputs);
+        const res = await createProduct();
+        clearForm();
+        Router.push({ pathname: `/product/${res.data.createProduct.id}` });
       }}
     >
-      <fieldset>
+      <DisplayError error={error} />
+      <fieldset disabled={loading} aria-busy={loading}>
         <label htmlFor="image">
           Image
           <input
